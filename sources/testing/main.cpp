290d6abd92558616ec5fbc347a829c2583a15215
sillsm <sillsm@gmail.com>
2014-02-24 20:30:41 -0800

    Failure detected- ready to get coding

--- sources/testing/main.cpp
+++ sources/testing/main.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 TEST(MathTest, TwoPlusTwoEqualsFour) {
-EXPECT_EQ(2 + 2, 5);
+EXPECT_EQ(2 + 2, 4);
 }
 int main(int argc, char **argv) {
 ::testing::InitGoogleTest( &argc, argv );
